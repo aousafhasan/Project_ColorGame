3cb83352d36e30dd11d7c0260823099fab12177c
Aousaf Hasan <aousaf.hasan@gmail.com>
2022-09-26 14:38:36 -0400

    First commit

---
+++ colorGame.js
@@ -0,0 +1,59 @@
+var colors = generateRandomColor(6);
+
+var squares = document.querySelectorAll(".square");
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1")
+
+colorDisplay.textContent = pickedColor;
+
+for(var i = 0; i < squares.length; i++){
+	// add initial colors to squares
+	squares[i].style.background = colors[i];
+
+	//add click listeners to squares
+	squares[i].addEventListener("click", function() {
+		//grab color of clicked squares
+		var clickedColor = this.style.background;
+		//compare color to pickedColor
+		if(clickedColor === pickedColor) {
+			messageDisplay.textContent = "Correct!!"
+			changeColors(clickedColor);
+			h1.style.backgroundColor = clickedColor;
+		} else {
+			this.style.backgroundColor = "#232323";
+			messageDisplay.textContent = "Try Again"
+		}
+	});
+}
+
+function changeColors(color){
+	for (var i = 0; i < squares.length; i++) {
+		squares[i].style.backgroundColor = color;
+	}
+}
+
+function pickColor(){
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
+}
+
+function generateRandomColor(num){
+	var arr = [];
+	// get random color and push into array
+	for(i=0; i< num; i++){
+		arr.push(randomColor())
+
+	}
+	return arr;
+}
+
+function randomColor(){
+	// pick r, g, b from 0-255
+	var r = Math.floor(Math.random() * 256);
+	var g = Math.floor(Math.random() * 256);
+	var b = Math.floor(Math.random() * 256);
+
+	return "rgb(" + r + ", " + g + ", " + b + ")";
+}
